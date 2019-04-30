0705ae6389201b83e5ccb5b2aef23e17a45a48f7
Devin Ragotzy <dragotzy7460@mail.kvcc.edu>
2019-04-30 07:13:51 -0400

    finished it works, only two loops

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use std::cmp;
+
+type Narray = Vec<Vec<usize>>;
+
+trait Show {
+    fn print_it(&self);
+}
+
+impl Show for Narray {
+    fn print_it(&self) {
+        print!("[ ");
+        for (i, v_slice) in self.iter().enumerate() {
+            if i == 0 {
+                println!("{:?},", v_slice);
+            } else {
+                println!("  {:?},", v_slice);
+            }
+        }
+        println!("]");
+    }
+}
+
+pub fn lev_distance(a: &str, b: &str) -> usize {
+    if a.is_empty() {
+        return a.chars().count();
+    }
+    if b.is_empty() {
+        return b.chars().count();
+    }
+
+    let b_size = b.chars().count() + 1; let a_size = a.chars().count() + 1;
+    let mut matrix: Narray = vec![vec![0; b_size]; a_size];
+    for (i, char_a) in a.chars().enumerate() {
+        matrix[i][0] = i;
+        for (j, char_b) in b.chars().enumerate() {
+            matrix[0][j] = j;
+            let subed = if char_a == char_b { 0 } else { 1 };
+            let mini = cmp::min(cmp::min(matrix[i + 1][j] + 1, matrix[i][j + 1] + 1), matrix[i][j] + subed);
+            matrix[i + 1][j + 1] = mini;
+        }
+    }
+    matrix[a_size-1][b_size-1]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let dist = lev_distance("Devin", "devin");
+        assert_eq!(dist, 1);
+        // println!("leven dist is: {}", dist);
+    }
+}
